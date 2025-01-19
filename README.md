334e2fb2b0cebed0f36fa5addaa0820d7186ff1f
M Wong <wongmt15@gmail.com>
2025-01-20 05:25:43 +1100

    20-1-25

--- README.md
+++ README.md
@@ -1,22 +1,6 @@
 ## Todo List, implemented by Svelte 5, SvelteKit, JavaScript, HTML and CSS
 ## In the current implementation, a new item can be added to list and a selected item can be deleted from list. 
 
-# create-svelte
-
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/main/packages/create-svelte).
-
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npm create svelte@latest
-
-# create a new project in my-app
-npm create svelte@latest my-app
-```
-
 ## Developing
 
 Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,35 +5,37 @@ let items = $state([])
 	
 function addItem(evt) {
     if (evt.key === 'Enter') {
-	  if (newItem.trim() !== '') {
-	    items.push({id:id++, item: newItem})
-        newItem = '';
-	  }
+        if (newItem.trim() !== '') {
+        items.push({id:id++, item: newItem})
+        newItem = ''	  }
     }
 }
 
-function removeItem(id) {
-    items = items.filter((t) => t.id !== id)
+function removeItem(todo) {
+    items = items.filter((t) => t !== todo)
 }
 
 function clickHandler() {
     if (newItem.trim() !== '') {
-      items.push({id:id++, item: newItem})
-      newItem = ''
-	}
+        items.push({id:id++, item: newItem})
+        newItem = ''
+    }
 }
 </script>
 
-<h3> To-Do list implemeted by Svelte 5 and SvelteKit</h3>
+<h3> To-Do list implemeted by Svelte 5</h3>
 
 <input class="myinput" type="text" bind:value={newItem} onkeydown={addItem} /> 
 <button class="mybutton" onclick= {clickHandler}>Add</button>
+{#if items.length === 0}
+<p>Todo list is empty. Please add todo item.</p>
+{/if}
 <h4> To-Do list: </h4>
 <ul>
-{#each items as {id, item} (id)}
+{#each items as todo (todo.id)}
    <li>
-      { item }
-      <button class="mybutton" onclick={()=>removeItem(id)}>Delete</button>
+      { todo.item }
+      <button class="mybutton" onclick={()=>removeItem(todo)}>Delete</button>
    </li>
 {/each}
 </ul>
